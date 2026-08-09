9cffbd63022d8d26d838a1222e5e7f0e625e66ea
webdev-steve01 <sylvesterpaulstephen1234@gmail.com>
2026-08-09 11:07:53 +0100

    add: replaced fallback:null to a loader

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,17 @@
 import Auth from "@/components/pages/auth/auth_home/Auth";
+import Loader from "@/components/shared/Loader";
 import { Suspense } from "react";
 
 function page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4 py-8 sm:px-6">
-      <Suspense fallback={null}>
+      <Suspense
+        fallback={
+          <div className="w-full h-screen flex items-center justify-center">
+            <Loader />
+          </div>
+        }
+      >
         <Auth />
       </Suspense>
     </div>
