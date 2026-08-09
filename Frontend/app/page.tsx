602b10113b756e01a388c127f0a86224bbf4f57d
webdev-steve01 <sylvesterpaulstephen1234@gmail.com>
2026-08-09 11:05:15 +0100

    fix: wrapped useSearchParams component in a suspense

--- Frontend/app/page.tsx
+++ Frontend/app/page.tsx
@@ -1,10 +1,12 @@
 import Auth from "@/components/pages/auth/auth_home/Auth";
-import React from "react";
+import { Suspense } from "react";
 
 function page() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4 py-8 sm:px-6">
-      <Auth />
+      <Suspense fallback={null}>
+        <Auth />
+      </Suspense>
     </div>
   );
 }
