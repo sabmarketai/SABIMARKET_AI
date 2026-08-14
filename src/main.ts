c44a4b97daae063c9fc8375fd942d7a4463f37ae
Lawliettt <aderinsolasamashimi@gmail.com>
2026-08-14 16:57:57 +0100

    more

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', "https://sabimarket-ai.vercel.app"],
     credentials: true,
   });
 
